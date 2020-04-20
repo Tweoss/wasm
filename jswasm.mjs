dc1dc529605bd6714e5427d7d9cbfb8e14f61d22
Francis Chua <hwymonkey@gmail.com>
2020-04-20 15:05:54 -0700

    modifications for fun

--- jswasm.mjs
+++ jswasm.mjs
@@ -1,4 +1,11 @@
 WebAssembly.instantiateStreaming(fetch('add.wasm'))
 .then(results => {
-    console.log(results.instance.exports.add(1, 2));
+    var i = 0;
+    do {
+        //wait(4000);
+        if (i%9999 == 1){
+            setTimeout(console.log(results.instance.exports.add(i, 2)),10000000);
+        }
+        i++;
+    } while (i<9999999);
 })
