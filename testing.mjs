d962d0f93cf011caf101454c9896206aa76f2610
Francis Chua <hwymonkey@gmail.com>
2020-05-09 18:22:30 -0700

    successfully alter canvas

--- testing.mjs
+++ testing.mjs
@@ -1,14 +1,14 @@
 // const fs = require('fs');
 
 const memory = new WebAssembly.Memory({
-    initial: 256,
-    maximum: 256
+	initial: 256,
+	maximum: 256
 });
 const heap = new Uint8Array(memory.buffer);
 const imports = {
-    env: {
-        memory: memory
-    }
+	env: {
+		memory: memory
+	}
 };
 
 // const wasmSource = new Uint8Array(fs.readFileSync("testing.wasm"));
@@ -18,21 +18,36 @@ const imports = {
 
 WebAssembly.instantiateStreaming(fetch('testing.wasm'),imports)
 .then(results => {
-    function reverse(arr) {
-        for (let i = 0; i < arr.length; ++i) {
-            heap[i] = arr[i];
-        }
-        results.instance.exports._reverse(0, arr.length);
-    
-        const result = [];
-        for (let i = 0; i < arr.length; ++i) {
-            result.push(heap[i]);
-        }
-        return result;
-    }
-    
-    const numbers = [14, 3, 77];
-    console.log(numbers, 'becomes', reverse(numbers));
+	function reverse(arr) {
+		for (let i = 0; i < arr.length; ++i) {
+			heap[i] = arr[i];
+		}
+		results.instance.exports._reverse(0, arr.length);
+	
+		const result = [];
+		for (let i = 0; i < arr.length; ++i) {
+			result.push(heap[i]);
+		}
+		return result;
+	}
+	
+	const numbers = [14, 3, 77];
+	console.log(numbers, 'becomes', reverse(numbers));
   
 })
+
+
+var canvas = document.getElementById('myCanvas');
+var ctx = canvas.getContext('2d');
+var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+var data = imageData.data;
+
+for (var i = 0; i < data.length; i += 4) {
+	data[i]     = 9   - data[i];     // red
+	data[i + 1] = 255 - data[i + 1]; // green
+	data[i + 2] = 255 - data[i + 2]; // blue
+	data[i + 3] = 255;               //alpha
+}
+ctx.putImageData(imageData, 0, 0);
+
 
