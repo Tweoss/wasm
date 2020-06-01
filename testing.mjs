f6974a8687404555bb8066b4953655d857e83edd
rohildshah <shah5963@gmail.com>
2020-05-31 18:10:56 -0700

    purge npm from project

--- testing.mjs
+++ testing.mjs
@@ -34,8 +34,11 @@ var data = imageData.data;
 // const wasmInstance = new WebAssembly.Instance(wasmModule, imports);
 
 
-WebAssembly.instantiateStreaming(fetch('testing.wasm'),imports)
-.then(results => {
+fetch('testing.wasm').then(response => 
+	response.arrayBuffer()
+).then(bytes => 
+  WebAssembly.instantiate(bytes, imports)
+).then(results => {
 	function reverse(arr) {
 		for (let i = 0; i < arr.length; ++i) {
 			heap[i] = arr[i];
@@ -119,6 +122,96 @@ WebAssembly.instantiateStreaming(fetch('testing.wasm'),imports)
 	const numbers = [14, 3, 77];
 	// console.log(numbers, 'becomes', reverse(numbers));
 	
-})
+});
+
+
+
+
+// WebAssembly.instantiateStreaming(fetch('testing.wasm'),imports)
+// .then(results => {
+// 	function reverse(arr) {
+// 		for (let i = 0; i < arr.length; ++i) {
+// 			heap[i] = arr[i];
+// 		}
+// 		results.instance.exports._reverse(0, arr.length);
+		
+// 		const result = [];
+// 		for (let i = 0; i < arr.length; ++i) {
+// 			result.push(heap[i]);
+// 		}
+// 		return result;
+// 	}
+// 	function testfunc(x,y,z,color){
+// 		return results.instance.exports.main(x,y,z,color);
+// 	}
+// 	results.instance.exports.max(2,5,0);
+
+// 	// for(i = 3; i<1003;i++){
+// 	// 	testfunc(i,10,3);
+// 	// }
+// 	var i, j, k;
+// 	var color
+// 	// // x = 1
+// 	// color = parseInt("FFFFF0F0",16);
+// 	// for (j = 21; j<321; j++){
+// 	// 	for (k = 21; k<321; k++){
+// 	// 		testfunc(21,j,k,color);
+// 	// 	}
+// 	// }
+// 	// // y = 1
+// 	// color = parseInt("FCBA03F0",16);
+// 	// for(i = 21; i<321; i+= .01){
+// 	// 	for (j = 21; j<321; j++){
+// 	// 		testfunc(i,21,j,color);
+// 	// 	}
+// 	// }
+// 	// // z = 1
+// 	// color = parseInt("00FFFFF0",16);
+// 	// for(i = 21; i<321; i+= .01){
+// 	// 	for (j = 21; j<321; j++){
+// 	// 		testfunc(i,j,21,color);
+// 	// 	}
+// 	// }
+// 	// // x = 301
+// 	// color = parseInt("FFFF00F0",16);
+// 	// for (j = 21; j<321; j++){
+// 	// 	for (k = 21; k<321; k++){
+// 	// 		testfunc(321,j,k,color);
+// 	// 	}
+// 	// }
+	
+// 	// // y = 301
+// 	// color = parseInt("F00FFFF0",16);
+// 	// for(i = 21; i<321; i+= .01){
+// 	// 	for (k = 21; k<321; k++){
+// 	// 		testfunc(i,321,k,color);
+// 	// 	}
+// 	// }
+// 	// // z = 301
+// 	// color = parseInt("F0F0F0F0",16);
+// 	// for(i = 21; i<321; i+= .01){
+// 	// 	for (j = 21; j<321; j++){
+// 	// 		testfunc(i,j,321,color);
+// 	// 	}
+// 	// }
+// 	color = parseInt("FFFFF0F0",16);
+// 	results.instance.exports.trishade(21,0,0,21,0,321,21,321,0,color);
+// 	results.instance.exports.trishade(21,321,321,21,321,0,21,0,321,color);
+	
+	
+
+// 	for (var i = 0; i < data.length; i += 4) {
+// 		data[i]     = heap[i]     //9   - data[i];     // red
+// 		data[i + 1] = heap[i + 1] //255 - data[i + 1]; // green
+// 		data[i + 2] = heap[i + 2] //255 - data[i + 2]; // blue
+// 		data[i + 3] = heap[i + 3] //255;               //alpha
+// 	}
+	
+// 	ctx.putImageData(imageData, 0, 0);
+// 	// console.log("PLACED");
+// 	const numbers = [14, 3, 77];
+// 	// console.log(numbers, 'becomes', reverse(numbers));
+	
+// })
 
 
